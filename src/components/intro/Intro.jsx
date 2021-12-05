43122acdaf3d5ae6c8451d3ae889592f232a973b
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-12-05 14:15:42 -0500

    intro page work

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,14 +1,25 @@
 import "./intro.scss"
+import {ArrowDownward} from "@mui/icons-material/";
 
 export default function intro() {
     return (
         <div className="intro" id="intro">
           <div className="left">
             <div className="imgContainer">
-              <img src="..\assets\me.jpg" alt="" />
+              <img src="../assets/me.jpg" alt="" />
             </div>
           </div>
-          <div className="right"></div>
+
+          <div className="right">
+            <div className="wrapper">
+              <h2>Hi! My name is</h2>
+              <h1>Brendan Rottmund</h1>
+              <h3>Full Stack Web Developmer</h3>
+            </div>
+            <a href="#portfolio">
+            <ArrowDownward className="Icon" id="arrowDownward" style={{ fontSize: 125 }} />
+            </a>
+          </div>
         </div>
     )
 }
