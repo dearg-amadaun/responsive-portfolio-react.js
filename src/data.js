8849c226abea3b13091d78b3ec47f9a2f6d67e1d
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-12-06 16:25:05 -0500

    Portfolio pages with placeholders

---
+++ src/data.js
@@ -0,0 +1,188 @@
+export const featuredPortfolio = [
+    {
+      id: 1,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/901",
+    },
+    {
+      id: 2,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/902",
+    },
+    {
+      id: 3,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/903",
+    },
+    {
+      id: 4,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/904",
+    },
+    {
+      id: 5,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/905",
+    },
+    {
+      id: 6,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/906",
+    },
+  ];
+  
+  export const webPortfolio = [
+    {
+      id: 1,
+      title: "App",
+      img: "https://picsum.photos/1200/907",
+    },
+    {
+      id: 2,
+      title: "App",
+      img: "https://picsum.photos/1200/908",
+    },
+    {
+      id: 3,
+      title: "App",
+      img: "https://picsum.photos/1200/909",
+    },
+    {
+      id: 4,
+      title: "App",
+      img: "https://picsum.photos/1200/801",
+    },
+    {
+      id: 5,
+      title: "App",
+      img: "https://picsum.photos/1200/802",
+    },
+    {
+      id: 6,
+      title: "App",
+      img: "https://picsum.photos/1200/803",
+    },
+  ];
+  
+  export const mobilePortfolio = [
+    {
+      id: 1,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/804",
+    },
+    {
+      id: 2,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/805",
+    },
+    {
+      id: 3,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/806",
+    },
+    {
+      id: 4,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/807",
+    },
+    {
+      id: 5,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/808",
+    },
+    {
+      id: 6,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/809",
+    },
+  ];
+  
+  export const designPortfolio = [
+    {
+      id: 1,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/910",
+    },
+    {
+      id: 2,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/912",
+    },
+    {
+      id: 3,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/913",
+    },
+    {
+      id: 4,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/914",
+    },
+    {
+      id: 5,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/915",
+    },
+    {
+      id: 6,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/916",
+    },
+  ];
+  
+  export const contentPortfolio = [
+    {
+      id: 1,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/917",
+    },
+    {
+      id: 2,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/918",
+    },
+    {
+      id: 3,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/919",
+    },
+    {
+      id: 4,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/920",
+    },
+    {
+      id: 5,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/921",
+    },
+    {
+      id: 6,
+      title: "App",
+      img:
+        "https://picsum.photos/1200/922",
+    },
+  ];
