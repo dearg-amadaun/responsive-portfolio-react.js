cfd043a77211063184255f108c22707534aa59f4
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-12-07 15:53:48 -0500

    testimonials pages start

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,23 @@ import "./testimonials.scss"
 export default function Testimonials() {
     return (
         <div className="testimonials" id="testimonials">
-          TESTIMONIALS
+          <h1>Testimonials</h1>
+          <div className="container">
+            <div className="card">
+              <div className="top">
+                <img src="assets/right-arrow.png" className="left" alt="" />
+                <img className="user" src="https://picsum.photos/500/500" alt="" />
+                <img className="right" src="assets/youtube.svg" alt="" />
+              </div>
+              <div className="center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa omnis deleniti repellendus qui praesentium inventore dolor?
+              </div>
+              <div className="bottom">
+                <h3>Name</h3>
+                <h4>Job Title</h4>
+              </div>
+            </div>
+          </div>
         </div>
     )
 }
