2ff81f4b84e3aa92aa0e4a5208df2f4f6b452955
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-12-07 13:43:03 -0500

    works  page

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,4 +1,6 @@
 import "./works.scss"
+import { Aod } from '@mui/icons-material/';
+
 
 export default function Works() {
     return (
@@ -7,7 +9,15 @@ export default function Works() {
                 <div className="container">
                     <div className="item">
                         <div className="left">
-                            <div className="leftContainer"></div>
+                            <div className="leftContainer">
+                                <div className="iconContainer">
+                                    <i id="aodIcon"><Aod style={{ fontSize: 30 }}/></i>
+                                </div>
+                                <h2>Title</h2>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perferendis laboriosam ut repudiandae soluta. Adipisci quos eveniet qui ad omnis.
+                                </p>
+                                <span>Projects</span>
+                            </div>
                         </div>
                         <div className="right"></div>
                     </div>
